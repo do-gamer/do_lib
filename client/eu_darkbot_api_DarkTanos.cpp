7a9563ec0f032c7468380aaf2d95c0a9661eb1da
do-gamer <208613570+do-gamer@users.noreply.github.com>
2026-02-15 23:57:06 +0700

    Make API version configurable via CMake

--- client/eu_darkbot_api_DarkTanos.cpp
+++ client/eu_darkbot_api_DarkTanos.cpp
@@ -72,7 +72,7 @@ JNIEXPORT jlong JNICALL Java_eu_darkbot_api_DarkTanos_getMemoryUsage
 JNIEXPORT jint JNICALL Java_eu_darkbot_api_DarkTanos_getVersion
   (JNIEnv *, jobject)
 {
-    return 7;
+    return API_VERSION;
 }
 
 JNIEXPORT void JNICALL Java_eu_darkbot_api_DarkTanos_keyClick
